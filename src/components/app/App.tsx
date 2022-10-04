9217f21c8a40e418193184227662dbab126c2ad8
RUBEN GALLEGO <ruben.gallegoramirezdeanton@telefonica.com>
2022-10-05 00:29:03 +0200

    create styles

---
+++ src/components/app/App.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import Post, {PostProps} from '../post/Post';
+import classNames from './App.module.css'
+
+// There are 2 waya to declarate a function:
+// function A ()
+//
+// or:
+//
+// const a = () => {};
+
+const posts: PostProps[] = [
+	{
+		title: 'example post 1',
+		description: 'some text 1',
+		author: 'ruben',
+		isPublished: true,
+	},
+	{
+		title: 'example post 2',
+		description: 'some text 2',
+		author: 'ruben',
+		isPublished: true,
+	},
+	{
+		title: 'example post 3',
+		description: 'some text 3',
+		author: 'ruben',
+		isPublished: false,
+	},
+]
+
+const App: React.FC = () => {
+	return (
+		<div className={classNames.wrapper}>
+			<h1 className={classNames.title}>Hello typescript!</h1>
+			<div className={classNames.postsWrapper}>
+				{posts.map((post) => (<Post key={post.title} {...post}/>))}
+			</div>
+		</div>
+	)
+}
+
+export default App;
